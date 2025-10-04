dbdd25325e2bc0ea6d93123f4db94c1e4130a8d4
Anastasiia <rzkvanastasia@gmail.com>
2025-10-04 18:16:05 +0300

    Implement check_sequence_length function.

--- additional_modules/module_filter_fastq.py
+++ additional_modules/module_filter_fastq.py
@@ -4,6 +4,15 @@ def calculate_gc_content(sequence: str) -> float:
         total_length = len(sequence)
 
         if total_length == 0:
-            return 0.0
+            return 'No sequence'
 
     return (gc_count / total_length) * 100
+
+def check_sequence_length(sequence: str, length_bounds = (0, 2**32)) -> bool:
+    seq_length = len(sequence)
+    if isinstance(length_bounds,(int, float)):
+        return 0 <= seq_length <= length_bounds
+    else:
+        lower, upper =  length_bounds
+        return lower <= seq_length <= upper
+
